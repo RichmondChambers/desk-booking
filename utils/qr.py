cdb44f5b3bfbb5daa6d99c55154f7c59d1aa66db
RichmondChambers <paul.richmond@richmondchambers.com>
2025-12-21 19:56:30 +0100

    Delete utils directory

--- utils/qr.py
+++
@@ -1,9 +0,0 @@
-import os
-import qrcode
-
-def generate_qr_codes(base_url: str, out_dir: str = "assets"):
-    os.makedirs(out_dir, exist_ok=True)
-    for desk_id in range(1, 16):
-        url = f"{base_url}/?checkin={desk_id}"
-        img = qrcode.make(url)
-        img.save(os.path.join(out_dir, f"qr_desk_{desk_id}.png"))
