97e942b8d1bc4a9e7a7c657ff31913716c4ec22d
RichmondChambers <paul.richmond@richmondchambers.com>
2025-12-28 17:23:59 +0100

    Update 2_Book_a_Desk.py

--- pages/2_Book_a_Desk.py
+++ pages/2_Book_a_Desk.py
@@ -243,8 +243,8 @@ grid_builder.configure_default_column(
     filter=False,
     cellStyle=cell_style,
     valueFormatter=value_formatter,
-    minWidth=110,
-    width=120,
+    minWidth=90,
+    width=100,
 )
 grid_builder.configure_column("Time", pinned="left", width=90)
 grid_builder.configure_grid_options(
